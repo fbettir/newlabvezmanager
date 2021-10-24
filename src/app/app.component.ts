13085b196c091638ce3e089cb5f2b06bc0061ef2
Bernadett Farkas <79795106+fbettir@users.noreply.github.com>
2021-10-24 18:02:14 +0200

    Update app.component.ts
    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Candidate } from './candidate';
 import { CandidateService } from './candidate.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import { Container } from '@angular/compiler/src/i18n/i18n_ast';
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,6 @@ import { NgForm } from '@angular/forms';
 export class AppComponent implements OnInit{
   title = 'newlabvezmanagerapp';
   public candidates: Candidate[];
-  //public editEmployee: Employee;
   
   constructor(private candidateService: CandidateService){}
   
@@ -30,4 +31,26 @@ export class AppComponent implements OnInit{
 	  }
 	 );
   }
+
+  public onOpenModal(candidate: Candidate, mode: string): void{
+
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addCandidateModal')
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#updateCandidateModal')
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteCandidateModal')
+    }
+    container.appendChild(button);
+    button.click();
+  }
+
+
 }
