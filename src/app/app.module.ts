393a791d206c762542e72fd6665e980b53f9597f
Bernadett Farkas <79795106+fbettir@users.noreply.github.com>
2021-10-24 15:17:48 +0200

    Update app.module.ts
    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CandidateService } from './candidate.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+	HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CandidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
