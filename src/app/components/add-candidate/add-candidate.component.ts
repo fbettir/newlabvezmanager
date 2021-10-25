a17dd9fcebffe37bc0d9326d9af7a3f73e71dcc1
Bernadett Farkas <79795106+fbettir@users.noreply.github.com>
2021-10-25 10:14:38 +0200

    Create add-candidate.component.ts
    helloú

---
+++ src/app/components/add-candidate/add-candidate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './add-candidate.component.html',
+  styleUrls: ['./add-candidate.component.css']
+})
+export class AddCandidateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
