0f1b37fef38f295c00a42749da6b872d3a226a30
Bernadett Farkas <79795106+fbettir@users.noreply.github.com>
2021-10-25 10:14:21 +0200

    Create candidate-list.component.ts
    hello

---
+++ src/app/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Candidate } from '../../candidate';
+import { CandidateService } from '../../candidate.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { Container } from '@angular/compiler/src/i18n/i18n_ast';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.css']
+})
+export class CandidateListComponent implements OnInit {
+  public candidates: Candidate[];
+
+
+  constructor(private candidateService: CandidateService) { }
+
+  ngOnInit(): void {
+    this.getCandidates();
+
+  }
+
+  public getCandidates(): void{
+	  this.candidateService.getCandidates().subscribe(
+	  (response: Candidate[]) => {
+		  this.candidates = response;
+	  },
+	  (error: HttpErrorResponse) => {
+		  alert(error.message);
+	  }
+	 );
+  }
+
+}
